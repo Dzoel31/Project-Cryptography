72c75a1cd08a0c6978f7ec94f766aa4cf47a44ca
Dzoel31 <dzulfikriadjmal@gmail.com>
2023-11-26 22:17:20 +0700

    update interface & requirement

--- dashboard-rsa.py
+++ dashboard-rsa.py
@@ -10,54 +10,54 @@ import rsa
 # Import streamlit library
 import streamlit as st
 
-st.title("RSA Cryptography")
-st.markdown('''
-            ### [Project Cryptography](https://github.com/Dzoel31/Project-Cryptography.git)
-            Kelompok 4:
-            - 2210511053 - Ananda Divana
-            - 2210511060 - M. Rizky Aulia
-            - 2210511063 - Daffa Bagus Maulana
-            - 2210511084 - Dzulfikri Adjmal
-            - 2210511087 - Diaz Saputra
-            ''')
-            
-st.header("Encrypt and Decrypt a message using RSA algorithm")
+st.set_page_config(page_title="RSA Cryptography", page_icon="🔐", layout='wide')
 
+st.title("🔑 :blue[RSA Cryptography] 🔑")
+
+st.header(":orange[Encrypt] and :orange[Decrypt] a message using RSA algorithm")
+
+with st.expander("Apa itu RSA?"):
+    st.markdown("""
+                RSA adalah algoritma kriptografi asimetris pertama yang digunakan untuk enkripsi dan tanda tangan digital. 
+                RSA adalah singkatan dari Rivest-Shamir-Adleman, pencipta algoritma tersebut. 
+                RSA berbasis faktorisasi, yang berarti bahwa kunci publik dan kunci privatnya terkait satu sama lain melalui fungsi faktorisasi.
+                """)
+    
 col1, col2 = st.columns(2)
 
 with col1:
     with st.form(key="encrypt"):
         st.subheader("Encrypt a message")
         text = st.text_area("Enter a message")
-        key_length = st.selectbox("Select length of key", [256, 512, 1024])
+        key_length = st.selectbox("Select length of key", [256, 512, 1024, 2048, 3072, 4096])
+
         submit = st.form_submit_button(label="Encrypt")
     
-    if text == "":
-        st.warning("Please enter a message to encrypt")
+        if text == "":
+            st.warning("Please enter a message to encrypt")
     
     @st.cache_data
     def generate_key(length):
-        return rsa.newkeys(length)
+        return rsa.newkeys(length, accurate=True)
 
     (public_key, private_key) = generate_key(key_length)
 
-    @st.cache_data
-    def encrypt_message(text, _public_key):
+    # @st.cache_data
+    def encrypt_message(text: str, _public_key):
         return rsa.encrypt(text.encode(), _public_key)
     
 
 # encrypt message
     if submit:
-        encrypted_message = encrypt_message(text, public_key)
-        st.write("Encrypted message: ", encrypted_message.hex())
-        
-        # public_key_str = f"Public key: {public_key.save_pkcs1()}"
-        # private_key_str = f"Private key: {private_key.save_pkcs1()}"
-        # st.write(public_key_str)
-        # st.write(private_key_str)
+        try:
+            encrypted_message = encrypt_message(text, public_key)
+            st.write("Encrypted message: ", encrypted_message.hex())
 
-        private_key_copy = private_key.save_pkcs1().hex()
-        st.text_area("Private key", value=private_key_copy, height=150, disabled=True)
+            private_key_copy = private_key.save_pkcs1().hex()
+            st.text_area("Private key", value=private_key_copy, height=150, disabled=True)
+        
+        except:
+            st.warning("Message to large, please enter a smaller message or use a larger key")
 
 with col2:
     with st.form(key="decrypt"):
@@ -80,3 +80,15 @@ with col2:
     except:
         st.warning("Please enter a valid encrypted message or check length of key")
 
+st.info("Setiap kali melakukan enkripsi, key akan digenerate secara otomatis. Silakan copy private key untuk melakukan dekripsi.")
+st.markdown('''
+            ### [Project Cryptography](https://github.com/Dzoel31/Project-Cryptography.git)
+            Kelompok 4:
+            - 2210511053 - Ananda Divana
+            - 2210511060 - M. Rizky Aulia
+            - 2210511063 - Daffa Bagus Maulana
+            - 2210511084 - Dzulfikri Adjmal
+            - 2210511087 - Diaz Saputra
+            ''')
+
+st.write("Group 4 Cryptograph © 2023")
